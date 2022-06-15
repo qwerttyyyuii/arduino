77cdb2caa5d4770dfee5d5a92ca4e65e68ac59ba
JK <qwerttyyyuii@gmail.com>
2022-06-15 19:35:52 +0900

    second

---
+++ armain.ino
@@ -0,0 +1,28 @@
+#include "rururu.h"
+
+Process P10;
+Process P05;
+Process test;
+Process S10;
+Process C10;
+
+void setup(void) {
+	Serial.begin(9600);
+	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)	
+	pinMode(10, OUTPUT);	
+		
+	P10.Properties(true, 0, 2, 0, 0, 1000, "P10", dosomething10);
+	P05.Properties(true, 0, 2, 0, 0, 500, "P05", dosomething05);
+	test.Properties(true, 0, 2, 0, 0, 333, "TST", dosomethingtest);
+	S10.Properties(true, 0, 0, 0, 0, 1000, "S10", dosomethingS10);
+	C10.Properties(true, 0, 0, 0, 0, 333, "C10", dosomethingC10);
+}
+
+void loop(void) {
+	P10.Start();
+	P05.Start();
+	test.Start();
+	S10.Start();
+    C10.Start();
+}
---
+++ armain/rururu.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+#include "rururu.h"
+// *.cpp로 쓸때는 사용해야함
+
+void dosomethingS10(Process *p) {
+	_printf("%d ", p->resetTime);
+}
+
+void dosomethingtest(Process *p) {
+	switch(p->cnt) {
+		case 0:			
+			break;
+		case 1:
+			break;
+	}
+}
+
+void dosomething10(Process *p) {	
+	switch(p->cnt) {
+		case 0:
+			digitalWrite(LED_BUILTIN, HIGH);		
+			break;
+		case 1:	
+			digitalWrite(LED_BUILTIN, LOW);			
+			break;
+	}
+}
+
+void dosomething05(Process *p) {
+	switch(p->cnt) {
+		case 0:
+			digitalWrite(9, HIGH);		
+			break;
+		case 1:	
+			digitalWrite(9, LOW);
+			break;
+	}
+}
+
+void dosomethingC10(Process *p) {
+	_printf("%s ", "C10C10 C10C10 C10C10");
+}
